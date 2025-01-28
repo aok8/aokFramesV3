e5af6048f82e14c4d5cfb25dd8163bd9a8b9d3b5
Alain Kouassi <aokouassi@gmail.com>
2025-01-27 19:50:47 -0800

    Scaffolding, need to figure out page layout past scrolling

--- src/routes/MainContent.svelte
+++ src/routes/MainContent.svelte
@@ -1,5 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+  onMount(() => {
+    window.scrollTo(0, 0);
+  });
+</script>
+
 <div class="content">
     <img src="/images/bg.jpg" alt="AOK Frames Star Pic" class="full-size-image" />
+</div>
+<div class="after-cover">
 </div>
 
 <style>
@@ -15,7 +24,16 @@
         width: 100%;
         height: 100%;
         object-fit: cover; /* Maintains aspect ratio while filling container */
-        z-index: 0; /* Ensures image stays behind other content */
+        z-index: 0; /* Increased z-index to appear above after-cover */
+        filter: drop-shadow(0 0 10px white);
+    }
+
+    .after-cover {
+        position: relative; /* Add this to establish a new stacking context */
+        width: 100%;
+        height: 100%;
+        background-color: #36454F;
+        z-index: -1; /* Ensure this stays below the image */
     }
   </style>
   

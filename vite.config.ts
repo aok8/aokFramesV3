2291d2788c08ba6e5ea49a8e2637a5aafb631b5f
Alain O. Kouassi <aokouassi@gmail.com>
2025-04-03 19:20:46 -0700

    Changes (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		{
+			name: 'copy-worker',
+			generateBundle() {
+				this.emitFile({
+					type: 'asset',
+					fileName: 'workers/r2-image-worker.js',
+					source: resolve(__dirname, 'workers', 'r2-image-worker.js'),
+				});
+			},
+		},
+	]
 });

e029f2a5dcb5b09b4a5016833ea71da4ccf8f12e
Alain O. Kouassi <aokouassi@gmail.com>
2025-04-03 18:51:09 -0700

    Testing adapter changes? other pr didnt stick (#3)

---
+++ src/theme/theme.d.ts
@@ -0,0 +1,11 @@
+export declare const theme: {
+    primary: string;
+    background: {
+        light: string;
+        dark: string;
+    };
+    text: {
+        primary: string;
+        light: string;
+    };
+};
--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,7 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		// Using Cloudflare adapter for Cloudflare Pages deployment
 		adapter: adapter()
 	},
 

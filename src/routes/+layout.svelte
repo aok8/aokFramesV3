7299c125099e495d79647a55ed481d223f6d6e75
Alain O. Kouassi <aokouassi@gmail.com>
2025-04-17 13:17:05 -0700

    Added preview info (#20)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,23 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+    <title>AOKFrames</title>
+    <meta name="description" content="Seattle based photographer capturing moments and telling stories through film and digital images." />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://aokframesv3.pages.dev/" />
+    <meta property="og:title" content="AOKFrames" />
+    <meta property="og:description" content="I'm a Seattle based photographer who shoots both film and digital. I have a passion for capturing moments and telling stories through images as well as trying to continuously improve while experiencing life to the fullest." />
+    <meta property="og:image" content="https://aokframesv3.pages.dev/directr2/constants/bg.jpg" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://aokframesv3.pages.dev/" />
+    <meta property="twitter:title" content="AOKFrames" />
+    <meta property="twitter:description" content="I'm a Seattle based photographer who shoots both film and digital. I have a passion for capturing moments and telling stories through images as well as trying to continuously improve while experiencing life to the fullest." />
+    <meta property="twitter:image" content="https://aokframesv3.pages.dev/directr2/constants/bg.jpg" />
+</svelte:head>
+
 <slot />
